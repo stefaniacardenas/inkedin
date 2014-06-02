bc12a22a1b06d046958f917b2cea9f8e20a2b70c
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-06-02 17:57:58 +0100

    messing around with the navigation bar, still need to fix the position of buttons

--- spec/features/home_feature_spec.rb
+++ spec/features/home_feature_spec.rb
@@ -2,9 +2,18 @@ require 'spec_helper'
 
 describe 'Flickagram Home'  do
 
-	it 'Should be desplayed when user are not logged in' do
-		visit '/'
-		expect(page).to have_content('Flickagram')
+	context 'when a user is not logged in' do 
+
+		it 'should be desplayed' do
+			visit '/'
+			expect(page).to have_content('Flickagram')
+		end
+
+			it 'Should be desplayed' do
+			visit '/'
+			expect(page).to have_content('Flickagram')
+		end
+
 	end
 
 end
